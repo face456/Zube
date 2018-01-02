74458413c3e57478d504c6f596dd249f5ca3a2f7
Rollin Donkey <info@rollindonkey.com>
2018-01-02 21:09:25 +0100

    added mongodb url

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-import mongodb from 'mongodb';
-
-export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": "mongodb://mark:123456@ds239217.mlab.com:39217/zubedatabase",
-  "bodyLimit": "100kb"
-}
