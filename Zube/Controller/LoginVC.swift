39009bc63fd705711f78e7e5cf8501f4da1d0e66
Rollin Donkey <info@rollindonkey.com>
2018-01-06 15:31:33 +0100

    finished the login process

--- Zube/Controller/LoginVC.swift
+++ Zube/Controller/LoginVC.swift
@@ -11,17 +11,49 @@ import UIKit
 class LoginVC: UIViewController {
 
     // MARK: Outlets
+    @IBOutlet weak var usernametxt: UITextField!
+    @IBOutlet weak var passwordtxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
+    // MARK: Actions
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernametxt.text , usernametxt.text != "" else {return}
+        guard let pass = passwordtxt.text , passwordtxt.text != "" else {return}
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
     }
+    
     @IBAction func createAccntBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        usernametxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: zubePurplePlaceholder])
+        passwordtxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: zubePurplePlaceholder])
     }
 }
 
---
+++ Zube/Controller/ProfileVC.swift
@@ -0,0 +1,46 @@
+//
+//  ProfileVC.swift
+//  Zube
+//
+//  Created by Philipp Jahn on 05.01.18.
+//  Copyright © 2018 Rollin Donkey. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    @IBAction func logoutPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView(){
+        userName.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        profileImg.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
